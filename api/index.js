e58aed9f614e9613eaf52a87fcaa0571d9be2cfe
Nacho Gallo <nacho.r.gallo@gmail.com>
2021-02-02 23:36:32 -0300

    Se instala mongoose y se agrega node-env

--- api/index.js
+++ api/index.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+const port = process.env.PORT || 3000;
+const env = require("node-env-file");
+env("../.env");
 
-const port = 3000;
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.get("*", (req, res) => {
   res.send({ message: "Hello World!" });
 });
 
 app.listen(port, () => {
-  console.log("Escuchando en el puerto 3000");
+  console.log(`Escuchando en el puerto ${port}`);
 });
+
+module.exports = app;
