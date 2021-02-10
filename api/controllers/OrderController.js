f572bf5923dd0f01b5c6f148a03ea1819e6c0ed2
Nachete <nacho.r.gallo@gmail.com>
2021-02-09 21:21:12 -0300

    Fix en controller

--- api/controllers/OrderController.js
+++ api/controllers/OrderController.js
@@ -1,12 +1,12 @@
 const Orders = require("../models/Orders");
 
-export const getAllOrders = (req, res) => {
+exports.getAllOrders = (req, res) => {
   Orders.find()
     .exec()
     .then((Orders) => res.status(200).send(Orders));
 };
 
-export const getOrderById = (req, res) => {
+exports.getOrderById = (req, res) => {
   Orders.findById(req.params.id)
     .exec()
     .then((order) => res.status(200).send(order));
